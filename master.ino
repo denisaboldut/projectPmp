c1ab6d5edf14d44eeee1eedee150c45d09072b03
denisaboldut <59513801+denisaboldut@users.noreply.github.com>
2020-01-13 19:48:19 +0200

    add project

---
+++ master.ino
@@ -0,0 +1,113 @@
+#define DEBUG true
+#include <Wire.h>
+volatile int x;
+void setup() {
+  
+ Wire.begin(); 
+ Serial.begin(115200);
+ Serial1.begin(115200);
+ pinMode(LED_BUILTIN, OUTPUT);
+ digitalWrite(LED_BUILTIN, LOW);
+ sendData("AT+RST\r\n", 2000, false); // resetare modul
+ sendData("AT+CWMODE=2\r\n", 1000, false); // configurare ca
+ //access point
+ sendData("AT+CIFSR\r\n", 1000, DEBUG); // citește adresa IP
+ sendData("AT+CWSAP?\r\n", 2000, DEBUG); // citește informația 
+
+ //SSID (nume rețea)
+ sendData("AT+CIPMUX=1\r\n", 1000, false); // configurare
+ //conexiuni multiple
+ sendData("AT+CIPSERVER=1,80\r\n", 1000, false); // pornire
+ //server pe port 80
+}
+void loop() {
+ 
+ if (Serial1.available()) {
+ if (Serial1.find("+IPD,")) {
+ delay(500);
+ int connectionId = Serial1.read() - 48; // functia
+ //read() returnează valori zecimale ASCII
+ // si caracterul ‘0’ are codul ASCII 48
+ String webpage = "<h1>Hello World!</h1><a href=\"/l0\"><button>On</button></a>";
+ String cipSend = "AT+CIPSEND=";
+ cipSend += connectionId;
+ cipSend += ",";
+ webpage += "<a href=\"/l1\"><button>off</button></a>";
+ webpage += "<a href=\"/l2\"><button>left</button></a>";
+  webpage += "<a href=\"/l3\"><button>right</button></a>";
+ 
+
+ if (readSensor() > 0) {
+ webpage += "<h2>Millis:</h2>";
+ webpage += readSensor();
+ }
+ cipSend += webpage.length();
+ cipSend += "\r\n";
+ sendData(cipSend, 100, DEBUG);
+ sendData(webpage, 150, DEBUG);
+
+ String closeCommand = "AT+CIPCLOSE=";
+ closeCommand += connectionId; //se adaugă
+ //identificatorul conexiunii
+ closeCommand += "\r\n";
+ sendData(closeCommand, 300, DEBUG);
+ }
+ }
+}
+String sendData(String command, const int timeout, boolean debug)
+{
+ String response = "";
+ Serial1.print(command); // trimite comanda la esp8266
+ long int time = millis();
+ while ((time + timeout) > millis()) {
+ while (Serial1.available()) {
+ char c = Serial1.read(); // citește caracter următor
+ response += c;
+ }
+ }
+  x=response.indexOf("/l0");
+ if (response.indexOf("/l0") != -1) {
+ digitalWrite(LED_BUILTIN, HIGH);
+ x=1;
+ Wire.beginTransmission(9); // transmitem spre device #9
+ 
+ Wire.write(x); // trimitem x
+ Wire.endTransmission(); 
+ }
+ if (response.indexOf("/l1") != -1) {
+  x=0;
+ Wire.beginTransmission(9); // transmitem spre device #9
+ 
+ Wire.write(x); // trimitem x
+ Wire.endTransmission();
+ digitalWrite(LED_BUILTIN, LOW);
+ }
+
+ if (response.indexOf("/l2") != -1) {
+
+ x=2;
+ Wire.beginTransmission(9); // transmitem spre device #9
+ 
+ Wire.write(x); // trimitem x
+ Wire.endTransmission();
+ digitalWrite(LED_BUILTIN, LOW);
+
+ }
+
+ if (response.indexOf("/l3") != -1) {
+   x=3;
+ Wire.beginTransmission(9); // transmitem spre device #9
+ 
+ Wire.write(x); // trimitem x
+ Wire.endTransmission();
+ digitalWrite(LED_BUILTIN, LOW);
+ }
+ 
+ if (debug) {
+ Serial.print(response);
+ }
+ return response;
+}
+unsigned long readSensor() {
+ return millis();
+}
---
+++ slave.ino
@@ -0,0 +1,132 @@
+#include <Servo.h>
+// Pinii motor 1
+#define mpin00 5
+#define mpin01 6
+// Pinii motor 2
+#define mpin10 3
+#define mpin11 11
+Servo srv;
+// Includem biblioteca necesară pentru I2C
+#include <Wire.h>
+int x = 0;
+const int trigPin = 9;
+const int echoPin = 10;
+// defines variables
+long duration;
+int distance;
+int state = 0;
+int potValue = 0;
+
+void setup() {
+ 
+  digitalWrite(mpin00, 0);
+ digitalWrite(mpin01, 0);
+ digitalWrite(mpin10, 0);
+ digitalWrite(mpin11, 0);
+ pinMode (mpin00, OUTPUT);
+ pinMode (mpin01, OUTPUT);
+ pinMode (mpin10, OUTPUT);
+ pinMode (mpin11, OUTPUT);
+ // pin LED
+ pinMode(13, OUTPUT);
+
+ 
+ // Pornim busul I2C ca și slave la adresa 9
+
+ 
+ Wire.begin(9);
+ // Atașăm o funcție care să se declanșeze atunci când primim
+ //ceva
+ Wire.onReceive(receiveEvent);
+ Serial.begin(9600);
+}
+//trimte serial 0 cand e oprit sau 1 contrar
+void receiveEvent(int bytes) {
+ x = Wire.read(); // citim un caracter din I2C
+}
+
+
+void StartMotor (int m1, int m2, int forward, int speed)
+{
+
+ if (speed==0) // oprire
+ {
+ digitalWrite(m1, 0); 
+ digitalWrite(m2, 0);
+ }
+ else
+ {
+ if (forward)
+ {
+ digitalWrite(m2, 0);
+ analogWrite (m1, speed); // folosire PWM
+ }
+ else
+ {
+ digitalWrite(m1, 0);
+ analogWrite(m2, speed);
+ }
+ }
+}
+
+void delayStopped(int ms)
+{
+ StartMotor (mpin00, mpin01, 0, 0);
+ StartMotor (mpin10, mpin11, 0, 0);
+ delay(ms);
+} 
+
+
+void loop() {
+
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+
+
+Wire.beginTransmission(9);
+Wire.write(distance);
+Wire.endTransmission();
+  
+
+ Serial.print(x);
+ if(x==0)
+ {
+ StartMotor (mpin00, mpin01, 0, 0);
+ StartMotor (mpin10, mpin11, 0, 0);
+ delay(500);
+ }
+ if(x==1)
+ {
+
+ StartMotor (mpin00, mpin01, 0, 128);
+ StartMotor (mpin10, mpin11, 0, 128);
+ delay (500);
+ }
+
+ if(x==2)
+ {
+ StartMotor (mpin00, mpin01, 1, 128);
+ StartMotor (mpin10, mpin11, 0, 128);
+ delay (1500);
+ x=1;
+ }
+
+ if(x==3)
+ {
+ StartMotor (mpin00, mpin01, 0, 128);
+ StartMotor (mpin10, mpin11, 1, 128);
+ delay (1500);
+x=1;
+ }
+}
